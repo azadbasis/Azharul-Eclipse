c7c425e538a697783b0784e17d54c722b24dd56d
Azharul Islam <Azharul Islam>
2016-04-06 02:23:39 -0700

    Simple Dictionary and Weather app using Volley

---
+++ WeatherApp/app/src/main/java/com/bitm/android/azharul/weatherapp/Interval.java
@@ -0,0 +1,13 @@
+package com.bitm.android.azharul.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Interval extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interval);
+    }
+}
