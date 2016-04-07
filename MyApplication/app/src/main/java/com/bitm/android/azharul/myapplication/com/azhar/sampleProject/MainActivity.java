13e48576101ba5fc0a09b0f4439792dd4fb23255
Azharul Islam <Azharul Islam>
2016-04-07 08:28:20 -0700

    update

---
+++ MyApplication/app/src/main/java/com/bitm/android/azharul/myapplication/com/azhar/sampleProject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bitm.android.azharul.myapplication.com.azhar.sampleProject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bitm.android.azharul.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
